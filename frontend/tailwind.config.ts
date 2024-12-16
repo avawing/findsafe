c44c9d76377e7294afb13ad01f3ec6c66498c4ec
avawingfield <avelinewingfield@gmail.com>
2024-12-16 14:54:07 -0500

    Skeleton Installed & "Working"

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,14 +1,35 @@
+// @ts-ignore
 import containerQueries from '@tailwindcss/container-queries';
+// @ts-ignore
 import forms from '@tailwindcss/forms';
+// @ts-ignore
 import typography from '@tailwindcss/typography';
+import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
+const config = {
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+				'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-
-	plugins: [typography, forms, containerQueries]
+	plugins: [
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton({
+			themes: { preset: [ "vintage" ] }
+		}),typography, forms, containerQueries
+	]
 } satisfies Config;
+
+export default config;
